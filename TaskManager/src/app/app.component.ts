3da69b9beae41b35275907a084ea6ed6a122bfe1
swarajs_mac <swaraj.sen2019@gmail.com>
2025-02-18 21:17:45 +0530

    Added the delete and toggle

--- TaskManager/src/app/app.component.ts
+++ TaskManager/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
--- TaskManager/src/app/services/task.service.ts
+++ TaskManager/src/app/services/task.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
-import { TASKS } from '../mock-task';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable} from 'rxjs'
 import { Task } from '../Task';
+
+const httpOptions = {
+  headers : new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTask(): Observable<Task[]>{
-    const task = of(TASKS);
-    return task
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
